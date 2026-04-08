91599d5d228ce4ea3668512670abd38d324c009e
vini0111 <vinicius.s.passos@hotmail.com>
2026-04-08 13:46:58 -0300

    Update dados.py

--- dados.py
+++ dados.py
@@ -13,9 +13,9 @@ jogos = [
     {"grupo": "A", "casa": "Gabriel", "fora": "Christian", "gols_casa": 0, "gols_fora": 0},
     {"grupo": "A", "casa": "Vinícius", "fora": "Arthur", "gols_casa": 1, "gols_fora": 0},
     {"grupo": "A", "casa": "Raoni", "fora": "Christian", "gols_casa": 1, "gols_fora": 5},
+    {"grupo": "A", "casa": "Vinícius", "fora": "Christian", "gols_casa": 0, "gols_fora": 1},
 
     {"grupo": "A", "casa": "Arthur", "fora": "Gabriel", "gols_casa": None, "gols_fora": None},
-    {"grupo": "A", "casa": "Vinícius", "fora": "Christian", "gols_casa": None, "gols_fora": None},
     {"grupo": "A", "casa": "Arthur", "fora": "Raoni", "gols_casa": None, "gols_fora": None},
     {"grupo": "A", "casa": "Vinícius", "fora": "Gabriel", "gols_casa": None, "gols_fora": None},
 
@@ -26,8 +26,8 @@ jogos = [
     {"grupo": "B", "casa": "Marcos", "fora": "Diego", "gols_casa": 0, "gols_fora": 2},
     {"grupo": "B", "casa": "Edgar", "fora": "Diego", "gols_casa": 0, "gols_fora": 0},
     {"grupo": "B", "casa": "Diego", "fora": "Jonathan", "gols_casa": 3, "gols_fora": 1},
+    {"grupo": "B", "casa": "Borello", "fora": "Marcos", "gols_casa": 0, "gols_fora": 0},
 
-    {"grupo": "B", "casa": "Borello", "fora": "Marcos", "gols_casa": None, "gols_fora": None},
     {"grupo": "B", "casa": "Edgar", "fora": "Marcos", "gols_casa": None, "gols_fora": None},
     {"grupo": "B", "casa": "Diego", "fora": "Borello", "gols_casa": None, "gols_fora": None},
     {"grupo": "B", "casa": "Edgar", "fora": "Jonathan", "gols_casa": None, "gols_fora": None},
